112bfe9a17ff4dfcf65a23e1e2e2413f1b471b91
Ananya-Priya <76117412+Ananya-Priya@users.noreply.github.com>
2021-01-18 20:57:53 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot{
+
+    constructor(body1,pointb){
+        var options=
+    {
+        bodyA: body1,
+        pointB: pointb,
+        stiffness: 0.4,
+        length: 10
+    }
+    
+    this.Sling=Constraint.create(options);
+    World.add(world,this.Sling); 
+    }
+    fly(){
+        this.Sling.bodyA=null;
+        
+    }
+
+    display(){
+        if(this.Sling.bodyA){
+        
+        var A=this.Sling.bodyA.position;
+        var B=this.Sling.pointB;
+        console.log(B.x)
+        line(A.x,A.y,B.x,B.y)
+        }
+    }
+}
